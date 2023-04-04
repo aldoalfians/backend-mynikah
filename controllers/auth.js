40810294457fe4eb4168cb7bc657c5f1d516c9dd
aldoalfians <aldoboetd908@gmail.com>
2023-04-05 00:21:24 +0700

    add register user

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,6 +31,28 @@ export const Login = async (req, res) => {
   res.status(200).json({ uuid, name, nik, email, role });
 };
 
+export const Register = async (req, res) => {
+  const { name, email, nik, password, confirmPassword, role } = req.body;
+  if (password !== confirmPassword)
+    return res
+      .status(400)
+      .json({ msg: "Password dan confirm passowrd tidak sama" });
+
+  const hashPassword = await argon2.hash(password);
+  try {
+    await User.create({
+      name,
+      email,
+      nik,
+      password: hashPassword,
+      role,
+    });
+    res.status(201).json({ msg: "Register Berhasil" });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
 export const userMe = async (req, res) => {
   if (!req.session.userId) {
     return res.status(401).json({ msg: "Mohon login ke akun Anda!" });
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { Login, logout, userMe } from "../controllers/auth.js";
+import { Login, logout, userMe, Register } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.get("/me", userMe);
 router.post("/login", Login);
+router.post("/regist", Register);
 router.delete("/logout", logout);
 
 export default router;
